b4655334ca48c654ea96182dd3886319d58fef55
Selim <selim-kune-do@hotmail.fr>
2021-03-31 22:28:10 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<x-app-layout>
+
+    @if (Route::has('login'))
+    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+        @auth
+            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+        @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+            @endif
+        @endauth
+    </div>
+@endif
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Accueil
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                salut
+            </div>
+        </div>
+    </div>
+</x-app-layout>
